3c98113571f1087d3b709d587a37d31481289889
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-02 01:42:29 +0300

    add description fields

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -21,18 +21,22 @@ export class UserResolver {
     private readonly usersService: UserService,
   ) {}
 
-  @Query(() => [PublicUserType])
+  @Query(() => [PublicUserType], { description: 'Tüm kullanıcıları bulur...' })
   async findUsers() {
     return this.usersService.getMany();
   }
 
-  @Query(() => PublicUserType)
+  @Query(() => PublicUserType, {
+    description: "Verilen ID'ye sahip kullanıcıyı bulur...",
+  })
   async findOneUser(@Args('data') data: GetUserInputType) {
     return this.usersService.get(data);
   }
 
   @Auth({ roles: ['admin'] })
-  @Mutation(() => PublicUserType)
+  @Mutation(() => PublicUserType, {
+    description: 'Kullanıcı oluşturur. Rolüm admin değilse izin vermez.',
+  })
   async createUser(@Args('data') data: CreateUserInputType) {
     const user = await this.usersService.create(data);
     await this.$pubSub.publish('on-user-created', user);
@@ -40,14 +44,18 @@ export class UserResolver {
   }
 
   @Auth({ roles: ['admin'] })
-  @Mutation(() => PublicUserType)
+  @Mutation(() => PublicUserType, {
+    description: 'Kullanıcı günceller. Rolüm admin değilse izin vermez.',
+  })
   async updateUser(@Args('data') data: UpdateUserInputType) {
     return this.usersService.update(data);
   }
 
   @Auth({ roles: ['admin'] })
   @UseGuards(GqlAuthGuard, RestrictToGuard)
-  @Mutation(() => String)
+  @Mutation(() => String, {
+    description: 'Kullanıcı siler. Rolüm admin değilse izin vermez.',
+  })
   async deleteUser(@Args('id', ParseMongoIdPipe) id: string) {
     await this.usersService.delete(id);
     return 'OK';
@@ -56,6 +64,8 @@ export class UserResolver {
   @Auth({ roles: ['admin'] })
   @Subscription(() => PublicUserType, {
     resolve: (payload) => payload,
+    description:
+      'Kullanıcı oluşturma olayını takibe alır. Her kullanıcı oluşturulduğunda haberim olur. Rolüm admin değilse izin vermez.',
   })
   async userCreated(@Args('token') token: string) {
     return this.$pubSub.asyncIterator<PublicUserType>('on-user-created');
