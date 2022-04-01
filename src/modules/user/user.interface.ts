2db7ac3dd639c49e233950b22a8552aa729cf112
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-02 02:04:00 +0300

    add comment sections

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -3,12 +3,24 @@ import mongoose from 'mongoose';
 export interface IUser extends mongoose.Document {
   id: string;
 
+  // İsim
   firstName: string;
+
+  // Soyisim
   lastName: string;
+
+  // Kullanıcı adı
   username: string;
+
+  // Şifre
   password: string;
+
+  // Rol
   role: 'admin' | 'user';
 
+  // Oluşturulma Tarihi
   createdAt: Date;
+
+  // Son Güncelleme Tarihi
   updatedAt: Date;
 }
