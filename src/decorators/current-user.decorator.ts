82949b327834bac8dd34f06fb75e1a1a15553706
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-02 01:33:39 +0300

    add comment sections

--- src/decorators/current-user.decorator.ts
+++ src/decorators/current-user.decorator.ts
@@ -1,9 +1,14 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { Request } from 'express';
 
+// Şu anki kullanıcı
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
+    // GraphQL'e uygun context'i alalım
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.user;
+
+    // Context içerisindeki request nesnesinden şu anki kullanıcıyı alalım
+    return ctx.switchToHttp().getRequest<Request>().user;
   },
 );
