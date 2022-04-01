63683e19df6f1cd2c3b193d8d6677afe4300350e
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-01 19:27:34 +0300

    add mongoid pipe

---
+++ src/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,16 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { isMongoId } from 'class-validator';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (!isMongoId(value)) throw new BadRequestException('id must be mongo id');
+
+    return value;
+  }
+}
