58492d42d798a0f1223de7341e3164e12903f79f
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-03 02:49:31 +0300

    implement like endpoint

--- src/modules/reaction/reaction.resolver.ts
+++ src/modules/reaction/reaction.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Auth } from 'src/decorators/auth.decorator';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { IUser } from '../user/user.interface';
+import { ReactionReturnType, ReactionInputType } from './reaction.graphql';
+import { ReactionService } from './reaction.service';
 
 @Resolver()
-export class ReactionResolver {}
+export class ReactionResolver {
+  constructor(private readonly reactionService: ReactionService) {}
+
+  @Auth()
+  @Mutation(() => ReactionReturnType)
+  async like(
+    @Args('data') data: ReactionInputType,
+    @CurrentUser() user: IUser,
+  ) {
+    console.log(data);
+    return this.reactionService.like(data.toUserId, user);
+  }
+}
