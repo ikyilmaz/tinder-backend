547f73a77f208f83c4a47084a1a6f8c7c2aa5a71
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-03 02:49:01 +0300

    implement methods

--- src/modules/reaction/reaction.service.ts
+++ src/modules/reaction/reaction.service.ts
@@ -24,29 +24,50 @@ export class ReactionService {
   }
 
   //   async like(toUserId: number, currentUser: User): Promise<Reaction | Match> {
-  async like(toUserId: string, currentUser: IUser) {
-    // const isMatch: Reaction = await this.isMatch(currentUser.id, toUserId);
-    // if (isMatch) {
-    //   const match = this.matchService.createMatch(toUserId, currentUser.id); // notify order
-    //   this.reactionRepository.remove(isMatch); //remove related reaction once match is created
-    //   return match;
-    // }
-    // const like = new Reaction();
-    // like.type = ReactionEnum.LIKE;
-    // like.toUserId = toUserId;
-    // like.fromUserId = currentUser.id;
-    // try {
-    //   await like.save();
-    // } catch (error) {
-    //   throw new ConflictException();
-    // }
-    // return like;
+  async like(toUserId: string, user: IUser) {
+    const isMatch = await this.isMatch(user.id, toUserId);
+    if (isMatch) {
+      // const match = this.matchService.createMatch(toUserId, currentUser.id); // notify order
+      // this.reactionModel.findByIdAndRemove(isMatch.id); //remove related reaction once match is created
+      // return match;
+    }
+    const like = await this.reactionModel.create({
+      toUserId,
+      fromUserId: user.id,
+      type: REACTION_TYPE.LIKE,
+    });
+
+    // TODO: Conflict Exception
+    return like;
   }
 
-  async isMatch(fromUserId: number, toUserId: number) {
-    //true if toUser liked me first
-    // return await this.reactionRepository.findOne({
-    //   where: { fromUserId: toUserId, toUserId: fromUserId, type: 'LIKE' },
-    // });
+  async superLike(toUserId: string, user: IUser) {
+    const isMatch = await this.isMatch(user.id, toUserId);
+    if (isMatch) {
+      // const match = this.matchService.createMatch(toUserId, currentUser.id); // notify order
+      // this.reactionModel.findByIdAndRemove(isMatch.id); //remove related reaction once match is created
+      // return match;
+    }
+    const like = await this.reactionModel.create({
+      toUserId,
+      fromUserId: user.id,
+      type: REACTION_TYPE.SUPERLIKE,
+    });
+
+    // TODO: Conflict Exception
+    return like;
+  }
+
+  async isMatch(fromUserId: string, toUserId: string) {
+    // true if toUser liked me first
+    return await this.reactionModel.findOne({
+      where: {
+        fromUserId: toUserId,
+        toUserId: fromUserId,
+        type: {
+          $or: [REACTION_TYPE.LIKE, REACTION_TYPE.SUPERLIKE],
+        },
+      },
+    });
   }
 }
