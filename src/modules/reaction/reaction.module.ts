4dfbe407d478d9ee55fe1177ca8b917d40e90ecf
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-03 04:24:17 +0300

    import match module

--- src/modules/reaction/reaction.module.ts
+++ src/modules/reaction/reaction.module.ts
@@ -4,9 +4,12 @@ import { ReactionResolver } from './reaction.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ModelNameEnum } from 'src/shared/constants/model-name.constant';
 import ReactionSchema from './reaction.schema';
+import { MatchService } from '../match/match.service';
+import { MatchModule } from '../match/match.module';
 
 @Module({
   imports: [
+    MatchModule,
     MongooseModule.forFeature([
       { name: ModelNameEnum.REACTION, schema: ReactionSchema },
     ]),
