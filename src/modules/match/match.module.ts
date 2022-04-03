480e69b1925887cc944bc591e1e5d6fc5d6a5cf2
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-03 04:23:51 +0300

    implement modules

--- src/modules/match/match.module.ts
+++ src/modules/match/match.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { MatchResolver } from './match.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ModelNameEnum } from 'src/shared/constants/model-name.constant';
+import MatchSchema from './match.schema';
+import { PubSub } from 'graphql-subscriptions';
+import UserSchema from '../user/user.schema';
 
 @Module({
-  providers: [MatchService, MatchResolver]
+  imports: [
+    MongooseModule.forFeature([
+      { name: ModelNameEnum.MATCH, schema: MatchSchema },
+      { name: ModelNameEnum.USER, schema: UserSchema },
+    ]),
+  ],
+  providers: [
+    MatchResolver,
+    MatchService,
+    {
+      provide: 'PUB_SUB',
+      useValue: new PubSub(),
+    },
+  ],
+  exports: [MatchService],
 })
 export class MatchModule {}
