dfbf957620c77a7a4b64169cd861d8a892bb230b
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-03 04:24:03 +0300

    implement methods

--- src/modules/match/match.resolver.ts
+++ src/modules/match/match.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Auth } from 'src/decorators/auth.decorator';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { GqlAuthGuard } from 'src/guards/gql-auth.guard';
+import { PublicUserType } from '../user/user.graphql';
+import { IUser } from '../user/user.interface';
+import { MatchReturnType } from './match.graphql';
+import { MatchService } from './match.service';
 
 @Resolver()
-export class MatchResolver {}
+export class MatchResolver {
+  constructor(private readonly matchService: MatchService) {}
+
+  @Auth()
+  @Query(() => [MatchReturnType], {
+    name: 'matches',
+  })
+  async getMatchedPartnersByUserId(@CurrentUser() user: IUser) {
+    return this.matchService.getMatchedPartnersByUserId(user.id);
+  }
+}
--- src/modules/match/match.service.ts
+++ src/modules/match/match.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { PubSub } from 'graphql-subscriptions';
+import mongoose from 'mongoose';
+import { ModelNameEnum } from 'src/shared/constants/model-name.constant';
+import { IUser } from '../user/user.interface';
+import { IMatch } from './match.interface';
 
 @Injectable()
-export class MatchService {}
+export class MatchService {
+  constructor(
+    @InjectModel(ModelNameEnum.MATCH)
+    private readonly matchModel: mongoose.Model<IMatch>,
+    @InjectModel(ModelNameEnum.USER)
+    private readonly userModel: mongoose.Model<IUser>,
+    @Inject('PUB_SUB') private readonly pubSub: PubSub,
+  ) {}
+
+  // Match Oluştur
+  async createMatch(firstUserId: string, secondUserId: string) {
+    const match = await this.matchModel.create({
+      firstUserId,
+      secondUserId,
+    });
+
+    // TODO: Conflict Exception
+
+    this.pubSub.publish('matchAdded', { matchAdded: match });
+    return match;
+  }
+
+  // Match olunan kişileri ID'lerine göre getirme
+  async getMatchedPartnersByUserId(userId: string) {
+    // Matchlerimi alıyorum. Birinci kullanıcı da olabilirim ikinci kullanıcı da...
+    const matches = await this.matchModel.find({
+      $or: [
+        {
+          firstUserId: userId,
+        },
+        {
+          secondUserId: userId,
+        },
+      ],
+    });
+
+    // Bu matchlerin ID'lerini alıyorum
+    const matchesIds = matches.map((match) => match.id);
+
+    // Bu matchlerdeki karşı tarafın ID'lerini alıyorum
+    const matchedUsersIds = matches.map((match) => {
+      // Match içerisinde ki birinci kullanıcı (firstUserId) ben değil isem birinci kullanıcı karşı taraf
+      // Eğer ben birinci kullanıcı isem ikinci kullanıcı (secondUserId) karşı taraf
+      return match.firstUserId.toString() != userId
+        ? match.firstUserId
+        : match.secondUserId;
+    });
+
+    // Karşı tarafta ki kullanıcıları aratıyorum
+    const matchedUsers = await this.userModel.find({
+      _id: { $in: matchedUsersIds },
+    });
+
+    // Ardından Match IDleri ile birlikte bu kullanıcıları dönüyorum...
+    return matchedUsers.map((user, i) => {
+      const userDoc = user.toJSON();
+      return { user: userDoc, matchId: matchesIds[i] };
+    });
+  }
+}
