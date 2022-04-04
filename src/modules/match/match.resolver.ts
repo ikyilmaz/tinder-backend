f5a64c8835688711df463764af7ba2dc9f994619
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-04 03:22:12 +0300

    add matchAdded subscription

--- src/modules/match/match.resolver.ts
+++ src/modules/match/match.resolver.ts
@@ -1,5 +1,6 @@
-import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Inject, UseGuards } from '@nestjs/common';
+import { Args, ID, Query, Resolver, Subscription } from '@nestjs/graphql';
+import { PubSub } from 'graphql-subscriptions';
 import { Auth } from 'src/decorators/auth.decorator';
 import { CurrentUser } from 'src/decorators/current-user.decorator';
 import { GqlAuthGuard } from 'src/guards/gql-auth.guard';
@@ -10,7 +11,10 @@ import { MatchService } from './match.service';
 
 @Resolver()
 export class MatchResolver {
-  constructor(private readonly matchService: MatchService) {}
+  constructor(
+    private readonly matchService: MatchService,
+    @Inject('PUB_SUB') private readonly pubSub: PubSub,
+  ) {}
 
   @Auth()
   @Query(() => [MatchReturnType], {
@@ -18,5 +22,22 @@ export class MatchResolver {
   })
   async getMatchedPartnersByUserId(@CurrentUser() user: IUser) {
     return this.matchService.getMatchedPartnersByUserId(user.id);
+  }
+
+  // @ResolveProperty((returns) => [Message])
+  // async messages(@Parent() match: Match): Promise<Message[]> {
+  //   return await this.messageService.getMessagesByMatchId(match.id);
+  // }
+
+  @Auth()
+  @Subscription(() => MatchReturnType, {
+    filter: (payload, variables: { token: string }, context) => {
+      console.log(context);
+      console.log('p', payload.matchAdded.firstUserId, variables.token);
+      return payload.matchAdded.firstUserId === variables.token;
+    },
+  })
+  matchAdded(@Args('token') token: string) {
+    return this.pubSub.asyncIterator('matchAdded');
   }
 }
