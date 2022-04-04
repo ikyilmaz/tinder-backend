7f5b6c86af8524796a369e7d35209e5647a5ed66
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-04 03:21:48 +0300

    add pubsub module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,16 +4,13 @@ import { UserResolver } from './user.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import UserSchema from './user.schema';
 import { PubSub } from 'graphql-subscriptions';
+import { PubSubModule } from '../pub-sub/pub-sub.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  providers: [
-    UserService,
-    UserResolver,
-    {
-      provide: 'PUB_SUB',
-      useValue: new PubSub(),
-    },
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    PubSubModule,
   ],
+  providers: [UserService, UserResolver],
 })
 export class UserModule {}
