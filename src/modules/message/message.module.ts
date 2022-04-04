afe5d64b15e25ecc76d17641c6469836f0e8fc6c
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-04 03:58:28 +0300

    import matchschema

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -4,11 +4,13 @@ import { MessageResolver } from './message.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ModelNameEnum } from 'src/shared/constants/model-name.constant';
 import MessageSchema from './message.schema';
+import MatchSchema from '../match/match.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ModelNameEnum.MESSAGE, schema: MessageSchema },
+      { name: ModelNameEnum.MATCH, schema: MatchSchema },
     ]),
   ],
   providers: [MessageService, MessageResolver],
